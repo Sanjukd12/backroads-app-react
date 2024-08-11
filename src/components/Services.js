1d23f5fa362a64ec55ad017af88e8fab6fc5d358
Sanjeev Kumar <108565766+Sanjukd12@users.noreply.github.com>
2024-08-11 22:57:16 +0530

    Dynamic Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,51 +1,28 @@
 import React from "react";
 import Title from "./Title";
+import { services } from "../data";
 
 const Services = () => {
   return (
     <>
       <section className="section services" id="services">
         <Title title="our" subTitle="services" />
-       
+
         <div className="section-center services-center">
-          <article className="service">
-            <span className="service-icon">
-              <i className="fas fa-wallet fa-fw"></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">saving money</h4>
-              <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
-              </p>
-            </div>
-          </article>
-
-          <article className="service">
-            <span className="service-icon">
-              <i className="fas fa-tree fa-fw"></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">endless hiking</h4>
-              <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
-              </p>
-            </div>
-          </article>
-
-          <article className="service">
-            <span className="service-icon">
-              <i className="fas fa-socks fa-fw"></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">amazing comfort</h4>
-              <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
-              </p>
-            </div>
-          </article>
+          {services.map((link) => {
+            const { id, icon, title, text } = services;
+            return (
+              <article className="service" key={link.id}>
+                <span className="service-icon">
+                  <i className={link.icon}></i>
+                </span>
+                <div className="service-info">
+                  <h4 className="service-title">{link.title}</h4>
+                  <p className="service-text">{link.text}</p>
+                </div>
+              </article>
+            );
+          })}
         </div>
       </section>
     </>
